9f324038f27dde7fae72a100ca2e3a14450584a3
technian-srikanth <banothu.srikanth@technians.com>
2026-04-17 13:22:53 +0530

    Initial commit

--- ts_task_templates/static/src/project_task_example.js
+++ ts_task_templates/static/src/project_task_example.js
@@ -3,9 +3,14 @@
 import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 import { renderToMarkup } from '@web/core/utils/render';
+import { markup } from "@odoo/owl";
+
+const greenBullet = markup(`<span class="o_status d-inline-block o_status_green"></span>`);
+const orangeBullet = markup(`<span class="o_status d-inline-block text-warning"></span>`);
+const star = markup(`<a style="color: gold;" class="fa fa-star"></a>`);
+const clock = markup(`<a class="fa fa-clock-o"></a>`);
 
 const projectExamples = registry.category("kanban_examples").get("project");
-
 projectExamples.examples.push({
     name: _t('Mobile App Development'),
     columns: [_t('Backlog'), _t('Design'), _t('Development'), _t('QA')],
@@ -13,4 +18,5 @@ projectExamples.examples.push({
     get description() {
         return renderToMarkup("project.example.generic");
     },
+    bullets: [greenBullet, orangeBullet, star, clock],
 });
